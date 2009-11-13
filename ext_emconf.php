3d3f3f4d304d778aaf5ee48ce556aa07d41330a1
nukem <nukem@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-13 15:20:37 +0000

    initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/gg_xajax/trunk@26500 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "gg_xajax"
+#
+# Auto generated 13-11-2009 18:00
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'GG xajax',
+	'description' => 'xajax 0.5 library',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '0.1.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Marco Malagoli [Gasser & Gasser]',
+	'author_email' => 'marco@gasser-gasser.ch',
+	'author_company' => 'Gasser & Gasser',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:56:{s:17:"copyright.inc.php";s:4:"e447";s:12:"ext_icon.gif";s:4:"5576";s:25:"xajax_core/legacy.inc.php";s:4:"fbd0";s:24:"xajax_core/xajax.inc.php";s:4:"5df5";s:39:"xajax_core/xajaxArgumentManager.inc.php";s:4:"a2a5";s:28:"xajax_core/xajaxCall.inc.php";s:4:"a68f";s:32:"xajax_core/xajaxCompress.inc.php";s:4:"fcdc";s:31:"xajax_core/xajaxControl.inc.php";s:4:"e82e";s:39:"xajax_core/xajaxLanguageManager.inc.php";s:4:"103e";s:30:"xajax_core/xajaxPlugin.inc.php";s:4:"a4c7";s:37:"xajax_core/xajaxPluginManager.inc.php";s:4:"27ac";s:31:"xajax_core/xajaxRequest.inc.php";s:4:"4d91";s:32:"xajax_core/xajaxResponse.inc.php";s:4:"308a";s:39:"xajax_core/xajaxResponseManager.inc.php";s:4:"bf9f";s:32:"xajax_core/xajax_lang_de.inc.php";s:4:"942f";s:32:"xajax_core/xajax_lang_nl.inc.php";s:4:"a8a8";s:57:"xajax_core/plugin_layer/xajaxCallableObjectPlugin.inc.php";s:4:"76c2";s:57:"xajax_core/plugin_layer/xajaxDefaultIncludePlugin.inc.php";s:4:"d6a6";s:48:"xajax_core/plugin_layer/xajaxEventPlugin.inc.php";s:4:"3a95";s:51:"xajax_core/plugin_layer/xajaxFunctionPlugin.inc.php";s:4:"7f50";s:49:"xajax_core/plugin_layer/xajaxScriptPlugin.inc.php";s:4:"f352";s:59:"xajax_core/plugin_layer/support/xajaxCallableObject.inc.php";s:4:"8f8d";s:50:"xajax_core/plugin_layer/support/xajaxEvent.inc.php";s:4:"6a07";s:57:"xajax_core/plugin_layer/support/xajaxUserFunction.inc.php";s:4:"ace4";s:14:"doc/manual.sxw";s:4:"a1f2";s:30:"xajax_controls/content.inc.php";s:4:"1cca";s:31:"xajax_controls/document.inc.php";s:4:"f8c7";s:27:"xajax_controls/form.inc.php";s:4:"9fbf";s:28:"xajax_controls/group.inc.php";s:4:"7b9d";s:27:"xajax_controls/misc.inc.php";s:4:"e3e0";s:32:"xajax_controls/structure.inc.php";s:4:"60cf";s:51:"xajax_controls/validate_HTML401TRANSITIONAL.inc.php";s:4:"e5d5";s:51:"xajax_controls/validate_XHTML10TRANSITIONAL.inc.php";s:4:"d55d";s:40:"xajax_plugins/response/googleMap.inc.php";s:4:"3f0b";s:43:"xajax_plugins/response/tableUpdater.inc.php";s:4:"b7e8";s:38:"xajax_plugins/response/tableUpdater.js";s:4:"c2d1";s:22:"xajax_js/xajax_core.js";s:4:"5705";s:35:"xajax_js/xajax_core_uncompressed.js";s:4:"6450";s:23:"xajax_js/xajax_debug.js";s:4:"026b";s:36:"xajax_js/xajax_debug_uncompressed.js";s:4:"b7dd";s:25:"xajax_js/xajax_lang_bg.js";s:4:"5339";s:38:"xajax_js/xajax_lang_bg_uncompressed.js";s:4:"5eff";s:25:"xajax_js/xajax_lang_de.js";s:4:"67ed";s:38:"xajax_js/xajax_lang_de_uncompressed.js";s:4:"803a";s:25:"xajax_js/xajax_lang_es.js";s:4:"3b88";s:38:"xajax_js/xajax_lang_es_uncompressed.js";s:4:"ee23";s:25:"xajax_js/xajax_lang_fr.js";s:4:"a840";s:38:"xajax_js/xajax_lang_fr_uncompressed.js";s:4:"ae0b";s:25:"xajax_js/xajax_lang_nl.js";s:4:"cf19";s:38:"xajax_js/xajax_lang_nl_uncompressed.js";s:4:"2400";s:25:"xajax_js/xajax_lang_tr.js";s:4:"3973";s:38:"xajax_js/xajax_lang_tr_uncompressed.js";s:4:"869d";s:24:"xajax_js/xajax_legacy.js";s:4:"cad5";s:37:"xajax_js/xajax_legacy_uncompressed.js";s:4:"dc6b";s:25:"xajax_js/xajax_verbose.js";s:4:"3dd0";s:38:"xajax_js/xajax_verbose_uncompressed.js";s:4:"9169";}',
+);
+
+?>
